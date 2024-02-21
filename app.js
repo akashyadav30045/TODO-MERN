6c4d8bca7400fb610d538e86fb4ed14b5523458c
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2024-02-21 07:35:15 +0530

    final

--- app.js
+++ app.js
@@ -6,27 +6,29 @@ require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// middleware
-
+// Middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
-// routes
-
+// Routes
 app.use('/api/v1/tasks', tasks);
 
+// Middleware for handling 404 errors
 app.use(notFound);
+
+// Middleware for error handling
 app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv');
-dotenv.config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const URL=process.env.MONGO_URI;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-
-const url = URL;
-const connectDB = () => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
-
-module.exports = connectDB
+module.exports = connectDB;
